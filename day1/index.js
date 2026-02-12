b8a656dd2cf7084ceaca80a0223bc6a4e2692872
Srishti Rawat <rawat.srishti1010@gmail.com>
2026-02-12 12:38:48 +0530

    all days

---
+++ day1/index.js
@@ -0,0 +1,11 @@
+const myPromise = new Promise((resolve, reject) => {
+  let marks = 78;
+  if (marks > 33) {
+    resolve("pass");
+  } else {
+    reject("fail");
+  }
+});
+myPromise
+  .then((msg) => console.log(msg))
+  .catch((error) => console.log("Some error occured", error));
